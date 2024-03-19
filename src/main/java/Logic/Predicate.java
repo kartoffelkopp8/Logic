9c2918ef8d987ad1d219b84998b7fc0a0edae63d
felix <felix.blenk@fau.de>
2024-03-19 18:24:34 +0100

    1

--- src/main/java/Logic/Predicate.java
+++ src/main/java/Logic/Predicate.java
@@ -1,24 +1,28 @@
 package Logic;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Predicate {
     private final char name;
     private int varConsAmmount;
     private boolean not;
-    private List<Variable> variables;
-    private List<Constant> constants;
+    private Map<Character, Constant> constants;
+    private Map<Character, Variable> variables;
+
 
     public Predicate(char name, int amount, boolean not) {
         this.name = name;
         this.not = not;
         this.varConsAmmount = amount;
-        this.variables = new ArrayList<>();
-        this.constants = new ArrayList<>();
+        this.variables = new HashMap<>();
+        this.constants = new HashMap<>();
     }
 
-    public Predicate(char name, int amount, boolean not, ArrayList<Constant> constants, ArrayList<Variable> variables) {
+    public Predicate(char name, int amount, boolean not, HashMap<Character, Constant> constants, HashMap<Character, Variable> variables){
+
         this.name = name;
         this.not = not;
         this.varConsAmmount = amount;
@@ -26,18 +30,18 @@ public class Predicate {
         this.constants = constants;
     }
 
-    public void addVariable(Variable var) {
+    public void addVariable(char name, Variable var) {
             if (variables.size() + constants.size() < varConsAmmount) {
-                variables.add(var);
+                variables.put(name, var);
             } else {
                 throw new IllegalStateException("max Ammount of Variables");
             }
 
     }
 
-    public void addConstant(Constant cons) {
+    public void addConstant(char name, Constant cons) {
         if (variables.size() + constants.size() < varConsAmmount) {
-            constants.add(cons);
+            constants.put(name, cons);
         } else {
             throw new IllegalStateException("max Ammount of Variables");
         }
